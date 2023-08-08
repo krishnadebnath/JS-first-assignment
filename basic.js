41a7dad64bed289518469feb45e7e3405e3c3881
krishnadebnath <krrishnadebnath51@gmail.com>
2023-08-09 00:02:56 +0600

    done 2 problem

--- basic.js
+++ basic.js
@@ -8,7 +8,7 @@
 //     }
 // }
 
-// var test = 8;
+// var test = 3;
 // var outout = cubeNumber(test);
 // console.log(outout); 
 
@@ -22,7 +22,7 @@
 
 // function matchFinder(string1, string2) {
 //     if (typeof string1 !== 'string' || typeof string2 !== 'string') {
-//         return;
+//         return "দুঃখিত, স্ট্রিং নয়"
 //     }
 
 //     if (string1.includes(string2)) {
@@ -41,12 +41,12 @@
 // // // 3rd problem
 
 
-// function sortMaker(totalDue) {
-//     if (!totalDueay.istotalDueay(totalDue) || totalDue.length !== 2) {
+// function sortMaker(arr) {
+//     if (!Array.isArray(arr) || arr.length !== 2) {
 //         return "Invalid Input";
 //     }
 
-//     let [num1, num2] = totalDue;
+//     let [num1, num2] = arr;
     
 //     if (typeof num1 !== 'number' || typeof num2 !== 'number') {
 //         return "Invalid Input";
@@ -54,26 +54,32 @@
 
 //     if (num1 === num2) {
 //         return "equal";
-//     } else if (num1 < num2) {
-//         return [num1, num2];
-//     } else {
+        
+//     } else if(num1 <0 || num2 <0){
+//         return "Invalid Input";
+//     }
+//     else if (num1 < num2) {
 //         return [num2, num1];
+//     } 
+//     else {
+//         return [num1, num2];
 //     }
 // }
-// var input1 = [4, -3];
-// var output = sortMaker(input1);
-// console.log(output);
+
+// var test = [2, 3];
+// var output = sortMaker(test);
+// console.log(output); 
 
 
 
 
 // 4th problem
 
-// function findAddress(object) {
-//     if (typeof object === 'object' && object !== null) {
-//        const street = object.street || '__';
-//        const house = object.house || '__';
-//        const society = object.society || '__';
+// function findAddress(obj) {
+//     if (typeof obj === 'object' && obj !== null) {
+//        const street = obj.street || '__';
+//        const house = obj.house || '__';
+//        const society = obj.society || '__';
         
 //         return `${street},${house},${society}`;
 //     } 
@@ -81,7 +87,7 @@
 
 // var test = {
 //     street: 10,
-//     house: "15A",
+//      house: "15A",
 //     society: "Earth Perfect"
 // };
 // var output = findAddress(test);
@@ -105,6 +111,6 @@
 // }
 
 // var poisha = [1, 5, 5];
-// var totalDue1 = 10;
-// var output = canPay(poisha, totalDue1);
+// var chipsPrice = 10;
+// var output = canPay(poisha, chipsPrice);
 // console.log(output);  
